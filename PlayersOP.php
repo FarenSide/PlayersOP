cb89a2b112457e7dd45a3cf2c804f42cd8945964
junyi00 <gohjunyi00@hotmail.com>
2013-06-26 21:59:13 +0800

    Create PlayersOP.php

---
+++ PlayersOP.php
@@ -0,0 +1,34 @@
+/* 
+name=PlayersOP
+description=Everyone who join is OP! 
+version=1.0
+author=Junyi00
+class=PlayersOP
+apiversion=9
+*/
+
+class PlayersOP implements Plugin {
+    private $api, $path;
+
+    public function __construct(ServerAPI $api, $server = false){
+        $this->api = $api;
+    }
+
+    public function init() {
+        $this->api->addHandler('player.join', array($this, 'OPPlayer'));
+    }
+
+    public function __destruct() { } 
+
+    public function OPPlayer($data, $event) {
+      switch($event) {
+    		case "player.join":
+    	
+    			$user = $data->username;
+    			$this->api->ban->commandHandler("op", $user, "console", false);
+    	        $this->api->chat->brodcast("$user is now OP!");
+       	}
+    	
+    }
+    
+}   
